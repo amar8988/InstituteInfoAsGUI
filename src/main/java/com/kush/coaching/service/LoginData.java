178c0e1163880210d2dc997ef04f7ac4d05bcbe0
Amarjeet Kumar <73456175+amar8988@users.noreply.github.com>
2022-03-02 17:38:54 +0530

    Add files via upload

--- src/main/java/com/kush/coaching/service/LoginData.java
+++ src/main/java/com/kush/coaching/service/LoginData.java
@@ -1,16 +1,19 @@
 package com.kush.coaching.service;
 
 
+import org.springframework.context.ConfigurableApplicationContext;
+
 import com.kush.coaching.entity.User;
 import com.kush.coaching.repository.UserRepository;
 
 public class LoginData {
 	
-	public User meth(int id, UserRepository userRepository) {
+	
+	public User meth(int id, ConfigurableApplicationContext configContext) {
 		User user=null;
 		
 		try {
-			user = userRepository.findById(id).get();
+			user = ((UserRepository) configContext.getBean(UserRepository.class)).findById(id).get();
 		}catch(Exception e) {
 			e.getMessage();
 		}
