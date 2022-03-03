f9caf61893e1ac65956b44687640b8241be0355d
Amarjeet Kumar <73456175+amar8988@users.noreply.github.com>
2022-03-03 10:19:36 +0530

    Add files via upload

--- src/main/java/com/kush/coaching/repository/StudentRepository.java
+++ src/main/java/com/kush/coaching/repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.kush.coaching.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 
 import com.kush.coaching.entity.StudentDetail;
 
-public interface StudentRepository extends JpaRepository<StudentDetail, Integer> {
-
+public interface StudentRepository extends CrudRepository<StudentDetail, Integer> {
+	List<StudentDetail> findAllByStatus(String status);
 }
